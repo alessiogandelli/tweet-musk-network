324c797050fff00be1d09372586020fc776d5fda
alessiogandelli <alessiogandelli99@gmail.com>
2022-12-23 21:42:02 +0100

    recursive method for filling the graph

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # tweet-musk-network
+
+# extract the data using twitter api with tweepy 
+
+- get all tweet of elon musk 
+- get all tweet in reply to the poll of elon musk about resigning as ceo of twitter 
+
+i have used search recent tweets but it is not working properly 
+
+i extracted 400k tweets with the field conversation_id =  the one of the poll, at the moment of scraping twitter the comments were 399k 
+
+trying to build a conversation tree i discovered that not all the tweets were in the dataset 
+
+
+There are 188636 tweets that were directly replying to the poll, but there are 210364 tweets that were replying to a tweet that was replying to the poll. 
+
+in df_first there are the tweet that are replying to the original tweet and that have at least one reply
+
+There are 5163 tweet that were replying to a reply to the first level reply, there are 76457 tweet at the second level let's see how they are distributed 
+
+in df_second there are all the tweets that are replying to a tweet from df_first
+
+of these 5163 we only have data for 4724 
+
+so we have 4724 level 2 replies with 18234 replies 
+
+
+
+
+
+
+# create graph 
--- src/network.py
+++ src/network.py
@@ -22,24 +22,77 @@ musk_resig = 1604617643973124097
 
 g = ig.Graph(directed=True)
 mentions = []
-
+edges = []
+# distance 1 from musk
 g.add_vertex(str(musk_resig))
 for tweet_id, tweet in df.iterrows():
     g.add_vertex(str(tweet_id), **tweet.to_dict())
 
     if tweet['replied_to'] == musk_resig:
        edges.append((str(tweet_id), str(musk_resig)))
-        
 
-
-
-df_first = df[df['replied_to'] == 1604617643973124097]
-edges = []
-for tweet_id, tweet in df_first.iterrows():
-   edges.append((str(tweet_id), str(musk_resig)))
+# distance 2 from musk
 
 
 g.add_edges(edges)
+
+#%%
+# filter df to get only the tweets that replied to musk's tweet and have some replies 
+df_first = df[df['replied_to'] == musk_resig]
+df_first = df_first[df_first['reply_count'] > 0]
+
+# extract from df only rows that have replied to the previous ones, in particolar people that replies to a reply 
+df_second = df[df['replied_to'].isin(df_first.index)]
+edges2 = []
+
+for tweet_id, tweet in df_second.iterrows():
+    edges2.append((str(tweet_id), str(tweet['replied_to'])))
+
+
+df_third = df_second[df_second['reply_count'] > 0]
+df_third = df[df['replied_to'].isin(df_second.index)]
+
+df_4 = df_third[df_third['reply_count'] > 0]
+df_4 = df[df['replied_to'].isin(df_third.index)]
+
+df_5 = df_4[df_4['reply_count'] > 0]
+df_5 = df[df['replied_to'].isin(df_4.index)]
+
+#%%
+# recoursive function to explore the tree of replies
+
+
+df_first = df[df['replied_to'] == musk_resig]
+total_reply = 0
+
+def explore_tree(n, df_n, total_reply):
+
+    print('al livello ',n ,df_n.shape[0],'reply')
+    total_reply += df_n.shape[0]
+
+    if(df_n.shape[0] > 0):
+        
+        df_n = df_n[df_n['reply_count'] > 0]
+        print('al livello ',n ,df_n.shape[0], 'reply con reply')
+        print('biggest thread',df_n.value_counts('reply_count').tail(1).index.tolist()[0])
+        df_n = df[df['replied_to'].isin(df_n.index)]
+        edges = []
+        for tweet_id, tweet in df_n.iterrows():
+            edges.append((str(tweet_id), str(tweet['replied_to'])))
+
+        g.add_edges(edges)
+    
+        return explore_tree(n+1, df_n, total_reply)
+    else:
+        return total_reply
+
+
+explore_tree(1, df_first, total_reply)
+
+#levels 
+
+
+
 
 #%%
 vs = g.vs['name']
@@ -75,7 +128,7 @@ ig.plot(g_ego,
     vertex_size=0.1, 
     vertex_color='blue', 
     edge_color='grey', 
-    layout='kk')
+    layout='circular')
 
 # %%
 
@@ -83,3 +136,5 @@ ig.plot(g_ego,
 # get type of the attribute
 # https://igraph.org/python/doc/api/igraph.VertexSeq-class.html 
 g.vs['degree'] = g.degree()
+
+# get longest path
