552b2c798de61732c07d6d6e66a835bd8f81ab13
alessiogandelli <alessiogandelli99@gmail.com>
2022-12-24 15:24:42 +0100

    try sigma js

--- src/network.py
+++ src/network.py
@@ -18,6 +18,31 @@ musk_resig = 1604617643973124097
 # %%
 
 
+
+def explore_tree(n, df_n, total_reply):
+
+    print('al livello ',n ,df_n.shape[0],'reply')
+    total_reply += df_n.shape[0]# count total replies 
+
+    if(df_n.shape[0] > 0):
+        
+        df_n = df_n[df_n['reply_count'] > 0] # filter only the ones that have replies
+        print('al livello ',n ,df_n.shape[0], 'reply con reply') 
+        print('biggest thread',df_n.value_counts('reply_count').tail(1).index.tolist()[0])
+        df_n = df[df['replied_to'].isin(df_n.index)] # take from the main dataset the ones that replied to the ones in df_n
+        edges = []
+
+        #append all edges for this level 
+        for tweet_id, tweet in df_n.iterrows():
+            edges.append((str(tweet_id), str(tweet['replied_to'])))
+
+        g.add_edges(edges) # add edges to the graph
+    
+        return explore_tree(n+1, df_n, total_reply)
+    else:
+        return total_reply
+
+
 ''' g contains all the nodes and all the direct answer to musk's tweet (400001, 188636) '''
 
 g = ig.Graph(directed=True)
@@ -25,6 +50,7 @@ mentions = []
 edges = []
 # distance 1 from musk
 g.add_vertex(str(musk_resig))
+
 for tweet_id, tweet in df.iterrows():
     g.add_vertex(str(tweet_id), **tweet.to_dict())
 
@@ -33,60 +59,12 @@ for tweet_id, tweet in df.iterrows():
 
 # distance 2 from musk
 
-
 g.add_edges(edges)
 
-#%%
-# filter df to get only the tweets that replied to musk's tweet and have some replies 
-df_first = df[df['replied_to'] == musk_resig]
-df_first = df_first[df_first['reply_count'] > 0]
-
-# extract from df only rows that have replied to the previous ones, in particolar people that replies to a reply 
-df_second = df[df['replied_to'].isin(df_first.index)]
-edges2 = []
-
-for tweet_id, tweet in df_second.iterrows():
-    edges2.append((str(tweet_id), str(tweet['replied_to'])))
-
-
-df_third = df_second[df_second['reply_count'] > 0]
-df_third = df[df['replied_to'].isin(df_second.index)]
-
-df_4 = df_third[df_third['reply_count'] > 0]
-df_4 = df[df['replied_to'].isin(df_third.index)]
-
-df_5 = df_4[df_4['reply_count'] > 0]
-df_5 = df[df['replied_to'].isin(df_4.index)]
-
-#%%
-# recoursive function to explore the tree of replies
 
 
 df_first = df[df['replied_to'] == musk_resig]
 total_reply = 0
-
-def explore_tree(n, df_n, total_reply):
-
-    print('al livello ',n ,df_n.shape[0],'reply')
-    total_reply += df_n.shape[0]
-
-    if(df_n.shape[0] > 0):
-        
-        df_n = df_n[df_n['reply_count'] > 0]
-        print('al livello ',n ,df_n.shape[0], 'reply con reply')
-        print('biggest thread',df_n.value_counts('reply_count').tail(1).index.tolist()[0])
-        df_n = df[df['replied_to'].isin(df_n.index)]
-        edges = []
-        for tweet_id, tweet in df_n.iterrows():
-            edges.append((str(tweet_id), str(tweet['replied_to'])))
-
-        g.add_edges(edges)
-    
-        return explore_tree(n+1, df_n, total_reply)
-    else:
-        return total_reply
-
-
 explore_tree(1, df_first, total_reply)
 
 #levels 
@@ -94,25 +72,14 @@ explore_tree(1, df_first, total_reply)
 
 
 
-#%%
-vs = g.vs['name']
-
-
-for tweet_id, tweet in df.iterrows():
-    if(str(tweet['replied_to'] in vs)): 
-        edges.append((str(tweet_id), str(tweet['replied_to'])))
-
-g.add_edges(edges)
-
-#export graph to gml
-g.write_gml('resign_tweet.gml')
-
 
 
 '''g_replied contains all the nodes that have at least one reply, 
-the only edges are between the ones that replied to musk's tweet (42837, 5163)'''
+the only edges are between the ones that replied to musk's tweet (42837, 16980)'''
 g_replied = g.vs.select( reply_count_ne = 0 ).subgraph()
 
+
+g_replied1 = g_replied.vs.select( reply_count_ne = 1 ).subgraph() # more than 1 reply
 
 
 
@@ -123,12 +90,12 @@ g_ego = g_replied.vs.select( degree_ne = 0 ).subgraph()
 # %%
 #plot graph 
 fig, ax = plt.subplots(figsize=(100,100))
-ig.plot(g_ego, 
+ig.plot(g_replied1, 
     target = ax,
-    vertex_size=0.1, 
+    vertex_size=0.5, 
     vertex_color='blue', 
     edge_color='grey', 
-    layout='circular')
+    layout='fr')
 
 # %%
 
@@ -138,3 +105,50 @@ ig.plot(g_ego,
 g.vs['degree'] = g.degree()
 
 # get longest path
+
+# %%
+
+import os
+import json
+import igraph as ig
+
+
+def ig_to_json(graph, path):
+    assert isinstance(graph, ig.Graph)
+    nodes = []
+    edges = []
+
+    if not 'layout' in graph.attributes():
+        graph['layout'] = graph.layout_auto()
+
+    for v, coords in zip(graph.vs, graph['layout']):
+        v_id = str(v.index)
+        v_attributes = v.attributes()
+        v_label = v_attributes.pop('label', None)
+        if not v_label:
+            v_label = v_id
+        v_size = v_attributes.pop('size', None)
+        if v_size:
+            v_size = float(v_size)
+        v_x = coords[0]
+        v_y = coords[1]
+        node = dict(id=v_id, label=v_label, size=v_size, x=v_x, y=v_y, attributes=v_attributes)
+        nodes.append(node)
+
+    for e in graph.es:
+        e_id = str(e.index)
+        e_source = str(e.source)
+        e_target = str(e.target)
+        e_attributes = e.attributes()
+        e_size = e_attributes.pop('size', None)
+        if e_size:
+            e_size = float(e_size)
+        edge = dict(id=e_id, source=e_source, target=e_target, size=e_size, attributes=e_attributes)
+        edges.append(edge)
+
+    data = dict(nodes=nodes, edges=edges)
+    with open(path, 'w') as f:
+        json.dump(data, f, ensure_ascii=False)
+    return os.path.exists(path)
+
+# %%
